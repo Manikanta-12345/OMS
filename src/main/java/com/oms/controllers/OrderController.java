44ccc0edd4257465e01cdcb825a0a2ccf1908458
Manikanta-12345 <suryamutyala757@gmail.com>
2021-11-20 15:25:24 +0530

    sm

--- src/main/java/com/oms/controllers/OrderController.java
+++ src/main/java/com/oms/controllers/OrderController.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,16 @@ public class OrderController {
 	public ResponseEntity<Order> placeOrder(@RequestBody Order order) {
 		String orderId = UUID.randomUUID().toString();
 		order.setOrderId(orderId);
+		return new ResponseEntity<Order>(order, HttpStatus.CREATED);
+	}
+	@GetMapping(value = "/orderslist")
+	public ResponseEntity<Order> getOrders() {
+		String orderId = UUID.randomUUID().toString();
+		Order order=new Order();
+		order.setOrderId(orderId);
+		order.setItemName("DOLO-650");
+		order.setItemPrice(500);
+		order.setQuantity(2);
 		return new ResponseEntity<Order>(order, HttpStatus.CREATED);
 	}
 }
